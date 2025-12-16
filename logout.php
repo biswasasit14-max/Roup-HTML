a38bf1d600cd2c62802691230f279c5c3397f89c
Roupaswa <biswasasit14@gmail.com>
2025-12-16 22:08:14 +0530

    Create logout.php
    Signed-off-by: Roupaswa <biswasasit14@gmail.com>

---
+++ logout.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session
+if (session_destroy()) {
+    // Redirect to login page
+    header('Location: login.html?logout=1');
+} else {
+    echo "Error: Could not log out properly.";
+    echo '<br><a href="login.html">Return to login</a>';
+}
+exit();
+?>
